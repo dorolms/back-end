40d1edb8ade8b27459f2c0274b56cecc5ad0ab50
ahn2y <myhand1207@gmail.com>
2025-11-18 14:12:16 +0900

    feat:announcements

--- announcements/urls.py
+++ announcements/urls.py
@@ -4,11 +4,11 @@ from .views import AnnouncementListCreateView, RecentAnnouncementListView, Annou
 urlpatterns = [
     # 목록 GET, POST
     # /api/announcements/
-    path('/', AnnouncementListCreateView.as_view(), name='announcement-list-create'),
+    path('', AnnouncementListCreateView.as_view(), name='announcement-list-create'),
     # 최신 공지 5개 GET
     # /api/announcements/recent/
-    path('announcements/recent/', RecentAnnouncementListView.as_view(), name='announcement-recent'),
+    path('recent/', RecentAnnouncementListView.as_view(), name='announcement-recent'),
     # 상세 GET, PUT, DELETE
     # /api/announcements/1/
-    path('/<int:pk>/', AnnouncementDetailView.as_view(), name='announcement-detail'),
+    path('<int:pk>/', AnnouncementDetailView.as_view(), name='announcement-detail'),
 ]
